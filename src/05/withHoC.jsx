81093f53d7a49cec0ed037a9e734403677408579
YG_Desk <yg4236@ajou.ac.kr>
2020-07-30 13:10:48 +0900

    Day18 react_study Higher-Order Components

--- src/05/withHoC.jsx
+++ src/05/withHoC.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default function withHoC(WrappedComponent) {
+  const { displayName, name: componentName } = WrappedComponent;
+  const wrappedComponentName = displayName || componentName;
   return class WithHoC extends React.Component {
+    static displayName = `withHoC(${wrappedComponentName})`;
     render() {
       return <WrappedComponent {...this.props} />;
     }
