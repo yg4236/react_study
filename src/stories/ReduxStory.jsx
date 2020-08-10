c28678b429221ef9aa756b921901a3fd929ee582
YG_Desk <yg4236@ajou.ac.kr>
2020-08-10 20:20:20 +0900

    Day29 react_study Reducer

--- src/stories/ReduxStory.jsx
+++ src/stories/ReduxStory.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ReduxApp01 from '../07/ReduxApp01';
-
-storiesOf('ReduxApp', module).addWithJSX('기본 스토어 설정', () => <ReduxApp01 />);
+import ReduxApp02 from '../07/ReduxApp02';
+storiesOf('ReduxApp', module)
+  .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
+  .addWithJSX('기본 액션 호출', () => <ReduxApp02 />);
