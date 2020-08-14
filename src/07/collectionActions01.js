e1fc936c0d26c4fcc58c9a325c596f882e3bd99d
YG_Desk <yg4236@ajou.ac.kr>
2020-08-14 18:54:21 +0900

    Day33 react_stduy DB basic

---
+++ src/07/collectionActions01.js
@@ -0,0 +1,6 @@
+export const SET_COLLECTION = 'collection/SET_COLLECTION';
+
+export const setColletion = collection => ({
+  type: SET_COLLECTION,
+  payload: collection,
+});
--- src/stories/ReduxStory.jsx
+++ src/stories/ReduxStory.jsx
@@ -6,10 +6,12 @@ import ReduxApp02 from '../07/ReduxApp02';
 import ReduxApp03 from '../07/ReduxApp03';
 import AdvReduxApp01 from '../07/AdvReduxApp01';
 import AdvReduxApp02 from '../07/AdvReduxApp02';
+import AdvReduxApp03 from '../07/AdvReduxApp03';
 
 storiesOf('ReduxApp', module)
   .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
   .addWithJSX('기본 액션 호출', () => <ReduxApp02 />)
   .addWithJSX('기본 리듀서 구현', () => <ReduxApp03 />)
   .addWithJSX('다중 리듀서 설정', () => <AdvReduxApp01 />)
-  .addWithJSX('다중 액션 설정', () => <AdvReduxApp02 />);
+  .addWithJSX('다중 액션 설정', () => <AdvReduxApp02 />)
+  .addWithJSX('그래프 데이터베이스', () => <AdvReduxApp03 />);
