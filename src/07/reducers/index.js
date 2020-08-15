9bcceac86bf97f020e15463758d7bb0514da4581
YG_Desk <yg4236@ajou.ac.kr>
2020-08-15 21:45:15 +0900

    Day34 react_study DB revise

--- src/07/reducers/index.js
+++ src/07/reducers/index.js
@@ -1,6 +1,7 @@
 import loading from './loadingReducer';
 import user from './userReducer';
-import collection from './collectionReducer02';
+//import collection from './collectionReducer02';
+import collection from './collectionReducer';
 
 export default {
   collection,
