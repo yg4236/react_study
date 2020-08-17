fc2039bf4cc889a76d08dfe703fe38c525be919c
YG_Desk <yg4236@ajou.ac.kr>
2020-08-17 23:57:50 +0900

    Day36 react_study Redux

--- src/stories/ReduxStory.jsx
+++ src/stories/ReduxStory.jsx
@@ -9,6 +9,9 @@ import AdvReduxApp02 from '../07/AdvReduxApp02';
 import AdvReduxApp03 from '../07/AdvReduxApp03';
 import AdvReduxApp04 from '../07/AdvReduxApp04';
 import AdvReduxApp05 from '../07/AdvReduxApp05';
+import AdvReduxApp06 from '../07/AdvReduxApp06';
+import AdvReduxApp07 from '../07/AdvReduxApp07';
+import AdvReduxApp08 from '../07/AdvReduxApp08';
 
 storiesOf('ReduxApp', module)
   .addWithJSX('기본 스토어 설정', () => <ReduxApp01 />)
@@ -18,4 +21,7 @@ storiesOf('ReduxApp', module)
   .addWithJSX('다중 액션 설정', () => <AdvReduxApp02 />)
   .addWithJSX('배열 액션 호출', () => <AdvReduxApp03 />)
   .addWithJSX('그래프DB 변환 작업', () => <AdvReduxApp04 />)
-  .addWithJSX('그래프DB 수정 액션 호출', () => <AdvReduxApp05 />);
+  .addWithJSX('그래프DB 수정 액션 호출', () => <AdvReduxApp05 />)
+  .addWithJSX('데이터 컴포넌트', () => <AdvReduxApp06 />)
+  .addWithJSX('데이터 컴포넌트 + 액션', () => <AdvReduxApp07 />)
+  .addWithJSX('데이터 컴포넌트 + 5가지 액션', () => <AdvReduxApp08 />);
