cfc6af522aa0a7f6ff3a86ee9d735ba2ffcb1aca
YG_Desk <yg4236@ajou.ac.kr>
2020-08-24 19:46:38 +0900

    Day43 react_study Project_Building

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import './doit-ui/app.css';
+import CoinApp from './08/CoinApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<CoinAppp />, document.getElementById('root'));
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
