38cffd3a7baa063d720039b981a814f0a03936c1
YG_Desk <yg4236@ajou.ac.kr>
2020-09-03 15:02:26 +0900

    Day53 react_study middleware

--- src/08/actions/transactionActions.js
+++ src/08/actions/transactionActions.js
@@ -4,6 +4,22 @@ import Api from '../Api';
 export const LOADING_TRANSACTION_LIST = 'transaction/LOADING_TRANSACTION_LIST';
 export const SET_TRANSACTION_LIST = 'transaction/SET_TRANSACTION_LIST';
 export const SET_ERROR = 'transaction/SET_ERROR';
+export const TRADE_COMPLETE = 'transaction/TRADE_COMPLETE';
+
+export function tradeComplete() {
+  return { type: TRADE_COMPLETE };
+}
+
+export function createTransaction(data, onComplete) {
+  return dispatch =>
+    Api.post('/transaction', data).then(
+      ({ data }) => {
+        dispatch(tradeComplete());
+        onComplete();
+      },
+      error => dispatch(setError(error.response.data.errorMessage)),
+    );
+}
 
 export function setError(errorMessage) {
   return {
--- src/08/store/configureStore.js
+++ src/08/store/configureStore.js
@@ -3,32 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers';
 import thunk from 'redux-thunk';
 import notifiactionEffects from '../middlewares/notificationEffects';
-
-// const customMiddleWare = store => nextRunner => action => {
-//   console.log('미들웨어에 전달된 액션 객체', action);
-//   console.log('리듀서 실행 전', store.getState());
-//   const result = nextRunner(action);
-//   console.log('리듀서 실행 후', store.getState());
-//   return result;
-// };
-
-// const customMiddleWare2 = store => nextRunner => action => {
-//   console.log('미들웨어2에 전달된 액션 객체', action);
-//   console.log('미들웨어2 실행 전', store.getState());
-//   const result = nextRunner(action);
-//   console.log('미들웨어2 실행 후', store.getState());
-//   return result;
-// };
-// export default initStates =>
-//   createStore(
-//     combineReducers(reducers),
-//     initStates,
-//     composeWithDevTools(applyMiddleware(customMiddleWare, customMiddleWare2)),
-//   );
+import transactionEffects from '../middlewares/transactionEffects';
 
 export default initState =>
   createStore(
     combineReducers(reducers),
     initState,
-    composeWithDevTools(applyMiddleware(thunk, notifiactionEffects)),
+    composeWithDevTools(applyMiddleware(thunk, notifiactionEffects, transactionEffects)),
   );
