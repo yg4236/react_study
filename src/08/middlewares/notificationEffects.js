091dbf5e4e883948fef64a7432b5d4293a133154
YG_Desk <yg4236@ajou.ac.kr>
2020-09-05 16:44:59 +0900

    Day55 react_study redux-pack

--- src/08/middlewares/notificationEffects.js
+++ src/08/middlewares/notificationEffects.js
@@ -1,18 +1,21 @@
 import { SET_ERROR } from '../actions/transactionActions';
 import { SHOW_NOTIFICATION, showMessage, hideMessage } from '../actions/notificationActions';
 import { debounce } from '../../02/debounce';
+import { KEY, LIFECYCLE } from 'redux-pack';
+import { FETCH_TRANSACTION_LIST } from '../actions/transactionPackActions';
 
 const debounceRunner = debounce(action => action(), 4000);
 
 export default store => nextRunner => action => {
-  const { type, payload } = action;
-  if (type === SET_ERROR) {
-    const { errorMessage } = payload;
-    store.dispatch(showMessage(errorMessage, true));
-  } else if (type === SHOW_NOTIFICATION) {
-    const hide = () => store.dispatch(hideMessage());
-    setTimeout(hide, 4000);
-    debounceRunner(hide);
+  const { type, meta } = action;
+  const result = nextRunner(action);
+  if (meta && meta.notification) {
+    const { success, error } = meta.notification;
+    if (success && meta[KEY.LIFECYCLE] === LIFECYCLE.SUCCESS) {
+      store.dispatch(showMessage(success));
+    } else if (error && meta[KEY.LIFECYCLE] === LIFECYCLE.FAILURE) {
+      store.dispatch(showMessage(error, true));
+    }
   }
   return nextRunner(action);
 };
